6be2a59e43acdd22c453977401af550596cc52c0
Dmitry Rodetsky <colirs.developer@gmail.com>
2020-04-11 03:09:48 -0600

    Animated sprite bunny

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,33 +1,32 @@
 extends KinematicBody2D
 
-const SPEED = 500
-const GRAVITY = 50
+const SPEED = 1000
+const GRAVITY = 80
 const UP = Vector2(0, -1)
-const JUMP_SPEED = 1000
+const JUMP_SPEED = 1500
 
 var motion = Vector2(0, 0)
 var move_left = false
 var move_right = false
-var on_floor = false
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	on_floor = is_on_floor()
 	apply_gravity()
 	move()
 	jump()
+	animate()
 	move_and_slide(motion, UP)
 	
 func apply_gravity():
-	if on_floor:
+	if is_on_floor():
 		motion.y = +5
 	else:
 		motion.y += GRAVITY
 		
 func jump():
-	if Input.is_action_just_pressed('jump') and on_floor:
+	if Input.is_action_just_pressed('jump') and is_on_floor():
 		motion.y -= JUMP_SPEED
 		
 func move():
@@ -41,3 +40,31 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
+		
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x != 0:
+		if motion.x < 0:
+			$AnimatedSprite.flip_h = true
+		else:
+			$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
